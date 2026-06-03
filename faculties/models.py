9cc75d858a84cad5608ca5531a338f6cad8021ba
axrorback <axrorback@gmail.com>
2026-06-03 11:56:19 +0500

    Update Faculty model: Add __str__ method and created_at field

--- faculties/models.py
+++ faculties/models.py
@@ -3,17 +3,15 @@ from django.db import models
 class Faculty(models.Model):
 
     slug = models.SlugField(unique=True)
-
     image = models.ImageField(upload_to='faculties')
-
     title_uz = models.CharField(max_length=255)
     title_en = models.CharField(max_length=255)
     title_ru = models.CharField(max_length=255)
-
     description_uz = models.TextField()
     description_en = models.TextField()
     description_ru = models.TextField()
-
     is_active = models.BooleanField(default=True)
-
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title_uz
