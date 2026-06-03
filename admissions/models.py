b100f635d474d83d50735a4cc1bf0fdde3e29593
axrorback <axrorback@gmail.com>
2026-06-03 11:56:50 +0500

    Add __str__ method to Scholarship model

--- admissions/models.py
+++ admissions/models.py
@@ -36,9 +36,10 @@ class Scholarship(models.Model):
     title_uz = models.CharField(max_length=255)
     title_en = models.CharField(max_length=255)
     title_ru = models.CharField(max_length=255)
-
     percentage = models.PositiveIntegerField()
-
     content_uz = models.TextField()
     content_en = models.TextField()
     content_ru = models.TextField()
+
+    def __str__(self):
+        return self.title_uz
