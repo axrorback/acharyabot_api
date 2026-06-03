3c76d1f7724ce92056959a28b411c1f2fae99756
axrorback <axrorback@gmail.com>
2026-06-03 12:05:09 +0500

    Add ContactMessage model with fields for user details, message content, and answer tracking

--- communication/models.py
+++ communication/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class ContactMessage(models.Model):
+    telegram_id = models.BigIntegerField()
+    full_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255,blank=True,null=True)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_answered = models.BooleanField(default=False)
+    answer = models.TextField(blank=True,null=True)
+    answered_at = models.DateTimeField(blank=True,null=True)
+    answered_by = models.ForeignKey("accounts.User",on_delete=models.SET_NULL,null=True)
+
+    def __str__(self):
+        return self.full_name
