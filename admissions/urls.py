fc7a0ca45e3ec4d14bf7dce292a682df1676efa5
axrorback <axrorback@gmail.com>
2026-06-03 16:54:22 +0500

    Add URL router and register viewsets for admissions module

---
+++ admissions/urls.py
@@ -0,0 +1,14 @@
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+router = DefaultRouter()
+
+router.register("processes",AdmissionProcessViewSet,basename="processes")
+
+router.register("fees",TuitionFeeViewSet,basename="fees")
+
+router.register("scholarships",ScholarshipViewSet,basename="scholarships")
+
+router.register("grants",GrantViewSet,basename="grants")
+
+router.register("documents",RequiredDocumentViewSet,basename="documents")
