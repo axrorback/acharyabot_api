99199533ec61ab0b22d664587f075bcc04f58c90
axrorback <axrorback@gmail.com>
2026-06-03 17:11:09 +0500

    Add OpenAPI schema and documentation endpoints to URLs configuration

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path , include
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/faculties/',include('faculties.urls')),
     path('api/communication/',include('communication.urls')),
     path('api/admission/',include('admissions.urls')),
+    path('api/applications/',include('applications.urls')),
+    path("api/schema/",SpectacularAPIView.as_view(),name="schema",),
+    path("api/docs/",SpectacularSwaggerView.as_view(url_name="schema"),name="swagger-ui",),
+    path("api/redoc/",SpectacularRedocView.as_view(url_name="schema"),name="redoc",),
 ]
