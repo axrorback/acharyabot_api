68e1884f27e6470169c48911910d26d74e3404a5
axrorback <axrorback@gmail.com>
2026-06-03 11:53:22 +0500

    Add TuitionFee model and __str__ methods for AdmissionProcess and TuitionFee

--- admissions/models.py
+++ admissions/models.py
@@ -9,7 +9,6 @@ class AdmissionProcess(models.Model):
     )
 
     process_type = models.CharField(max_length=50,choices=PROCESS_TYPES)
-
     title_uz = models.CharField(max_length=255)
     title_en = models.CharField(max_length=255)
     title_ru = models.CharField(max_length=255)
@@ -17,3 +16,16 @@ class AdmissionProcess(models.Model):
     content_uz = models.TextField()
     content_en = models.TextField()
     content_ru = models.TextField()
+
+    def __str__(self):
+        return self.title_uz
+
+
+class TuitionFee(models.Model):
+
+    faculty = models.ForeignKey("faculties.Faculty",on_delete=models.CASCADE)
+    national_fee = models.DecimalField(max_digits=12,decimal_places=2)
+    international_fee = models.DecimalField(max_digits=12,decimal_places=2)
+
+    def __str__(self):
+        return self.faculty.title_uz
