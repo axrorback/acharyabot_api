476d0ccd86ffc04094deffcc78b7857e27acfb02
axrorback <axrorback@gmail.com>
2026-06-03 12:07:30 +0500

    Add ContactInfo model with multilingual address fields, map URLs, working hours, and __str__ method

--- university/models.py
+++ university/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class ContactInfo(models.Model):
+
+    phone = models.CharField(max_length=50)
+    email = models.EmailField()
+    address_uz = models.TextField()
+    address_en = models.TextField()
+    address_ru = models.TextField()
+    google_map_url = models.URLField()
+    yandex_map_url = models.URLField()
+    working_hours = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "Contact Info"
