79f14d901f29545b48a75ac21cb35f1118e9ea4e
axrorback <axrorback@gmail.com>
2026-06-03 16:43:43 +0500

    Add AdmissionProcessViewSet with serializers for list and detail views

--- admissions/views.py
+++ admissions/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from .models import AdmissionProcess
+from .serializers import (
+    AdmissionProcessListSerializer,
+    AdmissionProcessDetailSerializer,
+)
+
+
+class AdmissionProcessViewSet(ReadOnlyModelViewSet):
+    queryset = AdmissionProcess.objects.all()
+
+    lookup_field = "process_type"
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AdmissionProcessListSerializer
+
+        return AdmissionProcessDetailSerializer
+
