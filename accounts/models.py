da592c0319d39add9cf8cc3aecf2695bb726ad37
axrorback <axrorback@gmail.com>
2026-06-03 11:49:36 +0500

    Add custom User model with role field and choices

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ("superadmin", "Super Admin"),
+        ("admission", "Admission Manager"),
+        ("callcenter", "Call Center"),
+        ("operator", "Operator"),
+    )
+    role = models.CharField(max_length=50,choices=ROLE_CHOICES,default="operator")
+
+    def __str__(self):
+        return self.username
