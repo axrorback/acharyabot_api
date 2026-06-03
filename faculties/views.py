d8257af86af3931fb3eed33f8c59bd2480de9fc3
axrorback <axrorback@gmail.com>
2026-06-03 13:47:57 +0500

    Add FacultyViewSet with list and detail serializers for active faculty filtering

--- faculties/views.py
+++ faculties/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from .models import Faculty
+from .serializers import (
+    FacultyListSerializer,
+    FacultyDetailSerializer
+)
+
+
+class FacultyViewSet(ReadOnlyModelViewSet):
+    queryset = Faculty.objects.filter(
+        is_active=True
+    )
+
+    lookup_field = "slug"
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return FacultyListSerializer
+
+        return FacultyDetailSerializer
