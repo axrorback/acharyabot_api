521946f8351159c2acbbce0279e21dd47809044e
axrorback <axrorback@gmail.com>
2026-06-03 11:50:59 +0500

    Add AdmissionProcess model with multilingual fields and process type choices

--- admissions/models.py
+++ admissions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class AdmissionProcess(models.Model):
+
+    PROCESS_TYPES = (
+        ("direct", "Direct Admission"),
+        ("auet", "AUET"),
+        ("dtm", "DTM"),
+    )
+
+    process_type = models.CharField(max_length=50,choices=PROCESS_TYPES)
+
+    title_uz = models.CharField(max_length=255)
+    title_en = models.CharField(max_length=255)
+    title_ru = models.CharField(max_length=255)
+
+    content_uz = models.TextField()
+    content_en = models.TextField()
+    content_ru = models.TextField()
