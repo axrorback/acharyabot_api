81a2296e7abb85e1fe466f35e13a5362f29079d1
axrorback <axrorback@gmail.com>
2026-06-03 11:50:09 +0500

    Add Faculty model with multilingual fields and image support

--- faculties/models.py
+++ faculties/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Faculty(models.Model):
+
+    slug = models.SlugField(unique=True)
+
+    image = models.ImageField(upload_to='faculties')
+
+    title_uz = models.CharField(max_length=255)
+    title_en = models.CharField(max_length=255)
+    title_ru = models.CharField(max_length=255)
+
+    description_uz = models.TextField()
+    description_en = models.TextField()
+    description_ru = models.TextField()
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
