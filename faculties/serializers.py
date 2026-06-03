c405be218aa2ef741137007a6321c2430a4ed60b
axrorback <axrorback@gmail.com>
2026-06-03 13:34:02 +0500

    Add FacultyListSerializer with multilingual title field

--- faculties/serializers.py
+++ faculties/serializers.py
@@ -36,3 +36,31 @@ class FacultySerializer(serializers.ModelSerializer):
     def get_description(self, obj):
         lang = self.get_language()
         return getattr(obj, f"description_{lang}")
+
+class FacultyListSerializer(serializers.ModelSerializer):
+    title = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Faculty
+        fields = (
+            "id",
+            "slug",
+            "title",
+        )
+
+    def get_language(self):
+        request = self.context.get("request")
+
+        if not request:
+            return "uz"
+
+        lang = request.query_params.get("lang", "uz")
+
+        if lang not in ["uz", "en", "ru"]:
+            lang = "uz"
+
+        return lang
+
+    def get_title(self, obj):
+        lang = self.get_language()
+        return getattr(obj, f"title_{lang}")
